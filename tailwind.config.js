eb5ee73786edf0211f49b67ad7bf1096d5561184
hamza essalhi <42662723+hamza-essalhi@users.noreply.github.com>
2024-04-08 02:27:28 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {
-      fontSize: {
-        'xsm': '12px', // Extra small screens
-      },
-    },
-  },
-  plugins: [],
-}
+
